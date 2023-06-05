0e8d5b5da97e04be3168adf42d8fd5bfd47a2887
Logan Kroll <logan@logans-mbp.lan>
2023-06-04 21:20:43 -0400

    VERSION 1.0.0

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer();
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://tictactoe-mu-wine.vercel.app/',
         methods: ['GET', 'POST'],
     },
 });
